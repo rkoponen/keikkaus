b2921f3261ba9e0f5bcda3296448fe39d2841b25
Ruupert Koponen <ruupert.koponen@gmail.com>
2023-11-24 14:32:58 +0200

    Add money wins/losses

--- components/result-list.tsx
+++ components/result-list.tsx
@@ -3,6 +3,7 @@ import SingleResult from "./single-result";
 import {
   LotteryResult,
   Result,
+  calculateWinAmount,
   checkResult,
   selectLotteryNumbers,
 } from "@/app/utils/lotto-utils";
@@ -18,30 +19,58 @@ const ResultList = (props: ResultListProps) => {
   const weeks = props.years * 52;
   const [resultList, setResultList] = useState<React.JSX.Element[]>([]);
   const [week, setWeek] = useState<number>(1);
+  const [wins, setWins] = useState<number>(0);
+  const [moneyUsed, setMoneyUsed] = useState<number>(0);
+  const [bestResult, setBestResult] = useState<Result | null>(null);
+
   const scrollContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const addNewItems = () => {
       if (week < weeks) {
+        let newWins = wins;
+        let newMoneyUsed = moneyUsed;
+        let newBestResult = bestResult;
         const newItems: React.JSX.Element[] = [];
         for (let i = week; i < week + 10 && i <= weeks; i++) {
-          newItems.push(<SingleResult rows={props.rows} week={i} />);
-          setWeek(i)
+          const lotteryResult = selectLotteryNumbers();
+
+          const playerResults: Result[] = props.rows.map((row) => {
+            const result = checkResult(row, lotteryResult);
+            newWins += result.winAmount;
+            newMoneyUsed += 1;
+
+            if (!newBestResult || result.winAmount > newBestResult.winAmount) {
+              newBestResult = result
+            }
+
+            return result;
+          });
+          newItems.push(
+            <SingleResult
+              lotteryResult={lotteryResult}
+              playerResults={playerResults}
+              rows={props.rows}
+              week={i}
+            />
+          );
         }
-    
+        setWeek(week + 10);
+        setMoneyUsed(newMoneyUsed);
+        setWins(newWins);
         setResultList((prevItems) => [...prevItems, ...newItems]);
+        setBestResult(newBestResult);
       } else {
         clearInterval(intervalId);
       }
     };
 
-    const intervalId = setInterval(addNewItems, 4);
+    const intervalId = setInterval(addNewItems, 50);
 
     return () => clearInterval(intervalId);
-  }, [week, weeks, props.rows]);
+  }, [week, weeks, props.rows, moneyUsed, wins]);
 
   useEffect(() => {
-    //Scroll to the bottom of the result scroll div
     if (scrollContainerRef.current) {
       scrollContainerRef.current.scrollTop =
         scrollContainerRef.current.scrollHeight;
@@ -52,6 +81,21 @@ const ResultList = (props: ResultListProps) => {
     <div className="w-1/2">
       <div className="overflow-y-scroll h-96" ref={scrollContainerRef}>
         <ul>{resultList}</ul>
+      </div>
+      <div className="flex flex-row justify-between">
+        <div className="bg-green-200 flex flex-col justify-center items-center p-8 w-1/2 rounded-tl-lg">
+          <p>Voitot</p>
+          <p className="text-xl">{wins}€</p>
+        </div>
+        <div className="bg-red-200 flex flex-col justify-center items-center p-8 w-1/2 rounded-tr-lg">
+          <p>Rahaa käytetty</p>
+          <p className="text-xl">{moneyUsed}€</p>
+        </div>
+      </div>
+      <div className="w-full bg-cyan-200 flex flex-col justify-center items-center p-8 rounded-b-lg">
+        <p className="">Paras tulos</p>
+        <p className="text-lg">{bestResult?.correctNumbers.length}{bestResult?.extraCorrect ? ' + 1' : ''} oikein</p>
+        <p className="text-lg">Voitto: {bestResult?.winAmount}€</p>
       </div>
     </div>
   );
--- components/single-result.tsx
+++ components/single-result.tsx
@@ -3,9 +3,11 @@ import { BsPlusLg } from "react-icons/bs";
 import {
   LotteryResult,
   Result,
+  calculateWinAmount,
   checkResult,
   selectLotteryNumbers,
 } from "@/app/utils/lotto-utils";
+import { Rowdies } from "next/font/google";
 
 interface LotteryNumberProps {
   number: number;
@@ -20,46 +22,48 @@ const LotteryNumber: React.FC<LotteryNumberProps> = ({ number }) => {
 };
 
 interface SingleResultProps {
+  lotteryResult: LotteryResult;
+  playerResults: Result[];
   rows: number[][];
   week: number;
 }
 
 const SingleResult: React.FC<SingleResultProps> = (props) => {
-  const [lotteryResult, setLotteryResult] = useState<LotteryResult>(
-    selectLotteryNumbers()
-  );
-
-  const playerResults: Result[] = props.rows.map((row) =>
-    checkResult(row, lotteryResult)
-  );
-
   return (
     <li key={props.week} className="my-6 w-full">
       <div className="flex flex-col">
         <span> Viikko: {props.week}</span>
         <span>Oikea rivi</span>
-        <div className="grid grid-cols-9 border p-2 justify-between items-center">
-          {lotteryResult?.numbers.map((number, index) => (
+        <div className="grid grid-cols-9 border p-2 justify-between items-center m-2">
+          {props.lotteryResult.numbers.map((number, index) => (
             <LotteryNumber key={index} number={number} />
           ))}
-          <BsPlusLg className="text-2xl mx-2" />
-          <LotteryNumber number={lotteryResult?.extraNumber!} />
+          <BsPlusLg className="text-2xl mr-auto ml-auto" />
+          <div className="border rounded-full text-center text-lg mx-2 bg-slate-100 border-green-400">
+            {props.lotteryResult.extraNumber}
+          </div>
         </div>
         <span>Rivisi</span>
         {props.rows.map((row, rowIndex) => {
+          const winAmount = props.playerResults[rowIndex].winAmount;
+          const win = winAmount > 0;
           return (
-            <div className="border" key={rowIndex}>
+            <div className="border p-2 rounded-xl mb-2" key={rowIndex}>
               <div className="grid grid-cols-9 p-2 justify-between items-center">
                 {row.map((number, index) => {
                   return (
                     <div
                       key={index}
                       className={`border rounded-full text-center text-lg mx-2 ${
-                        playerResults[rowIndex].correctNumbers.includes(
+                        props.playerResults[rowIndex].correctNumbers.includes(
                           number
-                        ) || number === lotteryResult.extraNumber
+                        )
                           ? "bg-green-200"
                           : "bg-slate-100"
+                      } ${
+                        number === props.lotteryResult.extraNumber
+                          ? "border border-green-400"
+                          : "border-none"
                       }`}
                     >
                       {number}
@@ -67,10 +71,17 @@ const SingleResult: React.FC<SingleResultProps> = (props) => {
                   );
                 })}
               </div>
-              <div>
-                <span className="font-regular">
-                  Osumat: {playerResults[rowIndex].correctNumbers.length}{" "}
-                  {playerResults[rowIndex].extraCorrect ? "+ 1" : ""}
+              <div className="flex flex-row items-center">
+                <span className="font-regular mx-6">
+                  Osumat: {props.playerResults[rowIndex].correctNumbers.length}{" "}
+                  {props.playerResults[rowIndex].extraCorrect ? "+ 1" : ""}
+                </span>
+                <span
+                  className={`${
+                    win ? "bg-green-200" : "bg-red-200"
+                  } mx-6 p-1 border rounded-lg`}
+                >
+                  {win ? `Voitto: ${winAmount}€` : "Ei voittoa"}
                 </span>
               </div>
             </div>
