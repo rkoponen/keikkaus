86ba632a647f39a58e2c2e61ca7aaad3cc10e070
Ruupert Koponen <ruupert.koponen@gmail.com>
2023-11-27 01:55:06 +0200

    Add react-window for efficient rendering

--- components/result-list.tsx
+++ components/result-list.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { createRef, useEffect, useRef, useState } from "react";
 import SingleResult from "./single-result";
 import {
   LotteryResult,
@@ -10,6 +10,9 @@ import {
 import { BsPlusLg } from "react-icons/bs";
 import { setTimeout } from "timers";
 import { render } from "@testing-library/react";
+import { VariableSizeList as List } from "react-window";
+import { CSSProperties } from "react";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 interface ResultListProps {
   key: number;
@@ -27,6 +30,9 @@ const ResultList = (props: ResultListProps) => {
   const [moneyUsed, setMoneyUsed] = useState<number>(0);
   const [bestResult, setBestResult] = useState<Result | null>(null);
 
+  const listRef = useRef<List>(null);
+  const rowHeights = useRef<{ [key: number]: number }>({});
+
   const scrollContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -37,7 +43,8 @@ const ResultList = (props: ResultListProps) => {
           let newMoneyUsed = moneyUsed;
           let newBestResult = bestResult;
           const newItems: React.JSX.Element[] = [];
-          const renderSpeed = props.years > 20 ? Math.round(props.years / 10) + 2 : 1
+          const renderSpeed = 1
+            // props.years > 20 ? Math.round(props.years / 10) + 2 : 1;
           for (let i = week; i < week + renderSpeed && i <= weeks; i++) {
             const lotteryResult = selectLotteryNumbers();
 
@@ -61,6 +68,7 @@ const ResultList = (props: ResultListProps) => {
                 playerResults={playerResults}
                 rows={props.rows}
                 week={i}
+                key={i}
               />
             );
           }
@@ -75,28 +83,74 @@ const ResultList = (props: ResultListProps) => {
         }
       };
       // const intervalDuration = Math.max(25000 / weeks, 50);
-      const intervalDuration = props.years < 20 ? 20 : 50
-      const intervalId = setInterval(addNewItems, intervalDuration);
+      const intervalDuration = props.years < 20 ? 20 : 50;
+      const intervalId = setInterval(addNewItems, 50);
 
       return () => clearInterval(intervalId);
     }
   }, [week, weeks, props.rows, moneyUsed, wins, bestResult, props]);
 
   useEffect(() => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollTop =
-        scrollContainerRef.current.scrollHeight;
+    // if (scrollContainerRef.current) {
+    //   scrollContainerRef.current.scrollTop =
+    //     scrollContainerRef.current.scrollHeight;
+    // }
+    if (resultList.length > 0) {
+      scrollToBottom();
     }
   }, [resultList]);
 
-  return (
-    <div className="">
-      <div
-        className="overflow-y-scroll h-96 mb-6 border p-2 rounded-xl"
-        ref={scrollContainerRef}
-      >
-        <ul>{resultList}</ul>
+  const Row = ({ index, style }: { index: number; style: CSSProperties }) => {
+    const rowRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+      if (rowRef.current) {
+        setRowHeight(index, rowRef.current.clientHeight);
+      }
+    }, [rowRef]);
+
+    return resultList[index] ? (
+      <div ref={rowRef} style={style} className="my-8">
+        {resultList[index]}
       </div>
+    ) : null;
+  };
+
+  const getRowHeight = (index: number) => {
+    return rowHeights.current[index] + 50 || 100;
+  };
+
+  const setRowHeight = (index: number, size: number) => {
+    if (listRef.current) {
+      listRef.current.resetAfterIndex(0);
+    }
+    rowHeights.current = { ...rowHeights.current, [index]: size };
+  };
+
+  const scrollToBottom = () => {
+    if (listRef.current) {
+      listRef.current.scrollToItem(resultList.length - 1, "end");
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-screen py-16">
+      <div className="flex h-1/2 w-full my-6 border p-4">
+        <AutoSizer>
+          {({ height, width }) => (
+            <List
+              height={height}
+              itemCount={resultList.length}
+              itemSize={getRowHeight}
+              width={width}
+              ref={listRef}
+            >
+              {Row}
+            </List>
+          )}
+        </AutoSizer>
+      </div>
+
       <div className="flex flex-row justify-between">
         <div className="bg-green-200 flex flex-col justify-center items-center p-8 w-1/2 rounded-tl-lg">
           <p>Voitot</p>
--- components/single-result.tsx
+++ components/single-result.tsx
@@ -15,7 +15,7 @@ interface LotteryNumberProps {
 
 const LotteryNumber: React.FC<LotteryNumberProps> = ({ number }) => {
   return (
-    <div className="border rounded-full text-center text-lg mx-2 bg-green-200">
+    <div className="border rounded-full w-10 h-10 flex items-center justify-center text-lg mx-2 bg-green-200">
       {number}
     </div>
   );
@@ -30,16 +30,16 @@ interface SingleResultProps {
 
 const SingleResult: React.FC<SingleResultProps> = (props) => {
   return (
-    <li key={props.week} className="my-6 w-full">
+    <div key={props.week} className="my-6 w-full">
       <div className="flex flex-col">
         <span> Viikko: {props.week}</span>
         <span>Oikea rivi</span>
-        <div className="grid grid-cols-9 border p-2 justify-between items-center m-2">
+        <div className="grid grid-cols-9 border p-2 justify-between items-center rounded-lg my-2">
           {props.lotteryResult.numbers.map((number, index) => (
             <LotteryNumber key={index} number={number} />
           ))}
           <BsPlusLg className="text-2xl mr-auto ml-auto" />
-          <div className="border rounded-full text-center text-lg mx-2 bg-slate-100 border-green-400">
+          <div className="border rounded-full w-10 h-10 flex items-center justify-center text-lg mx-2 bg-slate-100 border-green-400">
             {props.lotteryResult.extraNumber}
           </div>
         </div>
@@ -48,13 +48,16 @@ const SingleResult: React.FC<SingleResultProps> = (props) => {
           const winAmount = props.playerResults[rowIndex].winAmount;
           const win = winAmount > 0;
           return (
-            <div className="border p-2 rounded-xl mb-2" key={rowIndex}>
-              <div className="grid grid-cols-9 p-2 justify-between items-center">
+            <div
+              className="border p-2 rounded-xl mb-2 flex flex-row"
+              key={rowIndex}
+            >
+              <div className="grid grid-cols-9 justify-between items-center gap-7">
                 {row.map((number, index) => {
                   return (
                     <div
                       key={index}
-                      className={`border rounded-full text-center text-lg mx-2 ${
+                      className={`flex items-center justify-center border rounded-full text-center mx-2 w-8 h-8 ${
                         props.playerResults[rowIndex].correctNumbers.includes(
                           number
                         )
@@ -71,15 +74,15 @@ const SingleResult: React.FC<SingleResultProps> = (props) => {
                   );
                 })}
               </div>
-              <div className="flex flex-row items-center">
-                <span className="font-regular mx-6">
+              <div className="flex flex-row justify-between flex-grow items-center w-full">
+                <span className="">
                   Osumat: {props.playerResults[rowIndex].correctNumbers.length}{" "}
                   {props.playerResults[rowIndex].extraCorrect ? "+ 1" : ""}
                 </span>
                 <span
                   className={`${
                     win ? "bg-green-200" : "bg-red-200"
-                  } mx-6 p-1 border rounded-lg`}
+                  } p-2 border rounded-lg text-xs`}
                 >
                   {win ? `Voitto: ${winAmount}€` : "Ei voittoa"}
                 </span>
@@ -88,7 +91,7 @@ const SingleResult: React.FC<SingleResultProps> = (props) => {
           );
         })}
       </div>
-    </li>
+    </div>
   );
 };
 
