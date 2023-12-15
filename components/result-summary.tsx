e6e9c06915b8e4fd38fd64eb20195a909e078ea9
rkoponen <ruupert.koponen@gmail.com>
2023-12-15 11:50:00 +0200

    Refactor result summary to its own component

---
+++ components/result-summary.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { BestResult } from "./result-list";
+import { Button } from "./ui/button";
+
+interface ResultSummaryProps {
+  wins: number;
+  moneyUsed: number;
+  bestResult: BestResult;
+  handleClick: () => void;
+}
+
+const ResultSummary = (props: ResultSummaryProps) => {
+  return (
+    <div>
+      <div className="flex flex-row justify-between">
+        <div className="bg-green-200 flex flex-col justify-center items-center p-8 w-1/2 rounded-tl-lg">
+          <p>Voitot</p>
+          <p className="text-xl">
+            {props.wins.toLocaleString("fi-FI", {
+              style: "currency",
+              currency: "EUR",
+            })}
+          </p>
+        </div>
+        <div className="bg-red-200 flex flex-col justify-center items-center p-8 w-1/2 rounded-tr-lg">
+          <p>Rahaa käytetty</p>
+          <p className="text-xl">
+            {props.moneyUsed.toLocaleString("fi-FI", {
+              style: "currency",
+              currency: "EUR",
+            })}
+          </p>
+        </div>
+      </div>
+      <div className="w-full bg-cyan-200 flex flex-col justify-center items-center p-8 rounded-b-lg">
+        <p className="">Paras tulos</p>
+        <p className="text-lg">
+          {props.bestResult?.result.correctNumbers.length}
+          {props.bestResult?.result.extraCorrect ? " + 1" : ""} oikein
+        </p>
+        <p className="text-lg">
+          Voitto:{" "}
+          {props.bestResult?.result.winAmount.toLocaleString("fi-FI", {
+            style: "currency",
+            currency: "EUR",
+          })}
+        </p>
+        <Button onClick={props.handleClick}>Näytä</Button>
+      </div>
+    </div>
+  );
+};
+
+export default ResultSummary;
