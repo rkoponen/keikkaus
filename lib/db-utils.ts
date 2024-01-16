a10a463c02552dfb817de0333cd03e5be2a582e9
rkoponen <ruupert.koponen@gmail.com>
2024-01-16 18:35:25 +0200

    Update fetch method

--- lib/db-utils.ts
+++ lib/db-utils.ts
@@ -12,7 +12,7 @@ export const getScores = async (game: Games) => {
     .collection<Highscore>("highscores")
     .find({ game: game })
     .sort({ score: -1 })
-    .limit(100)
+    .limit(15)
     .toArray();
   return scores;
 };
