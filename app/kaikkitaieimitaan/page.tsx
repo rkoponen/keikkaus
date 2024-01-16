ee836bf5840b14ad02848b99e4c5cc98b6923f5a
rkoponen <ruupert.koponen@gmail.com>
2024-01-16 18:29:33 +0200

    Fix scores display

--- app/kaikkitaieimitaan/page.tsx
+++ app/kaikkitaieimitaan/page.tsx
@@ -6,15 +6,27 @@ export const dynamic = "force-dynamic";
 
 const AoNPage = () => {
   return (
+    // <main className="flex min-h-screen w-screen  flex-col items-center justify-between p-2">
+    //   <div className="relative z-10 flex w-full flex-col items-center justify-center font-mono text-sm">
+    //     <div className="static">
+    //       <div className="w-full">
+    //         <AonOptions />
+    //       </div>
+    //       <div className="right-0 top-0 m-6 hidden lg:absolute xl:block">
+    //         <Scoreslist game={Games.AllOrNothing} />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </main>
     <main className="relative flex min-h-screen flex-col items-center justify-between p-2">
       <div className="z-10 flex w-full flex-col items-center justify-center font-mono text-sm">
         {/* "flex flex-col-reverse gap-6 lg:flex-row lg:justify-between" */}
         <div className="flex flex-col justify-between lg:flex-row">
-          <div className="mx-6 w-96"></div>
+          <div className="mx-6 hidden w-96 xl:block"></div>
           <div className="">
             <AonOptions />
           </div>
-          <div className="mx-6 w-96">
+          <div className="mx-6 hidden w-96 xl:block">
             <Scoreslist game={Games.AllOrNothing} />
           </div>
         </div>
--- app/lotto/page.tsx
+++ app/lotto/page.tsx
@@ -12,11 +12,11 @@ const LottoPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <div className="flex flex-row justify-between">
-        <div className="mx-6 w-96"></div>
+        <div className="mx-6 hidden w-96 xl:block"></div>
         <div>
           <LottoGame />
         </div>
-        <div className="mx-6 w-96">
+        <div className="mx-6 hidden w-96 xl:block">
           <Scoreslist game={Games.Lotto} />
         </div>
       </div>
